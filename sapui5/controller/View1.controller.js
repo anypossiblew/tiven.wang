407bbaafb783af78a17d308d4d7b1b89dfdbefe0
tiven.wang <anypossible.w@gmail.com>
2016-08-23 18:19:20 +0800

    Profile of SAPUI5
    Profile of SAPUI5

---
+++ sapui5/controller/View1.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function(Controller, JSONModel, ResourceModel) {
+	"use strict";
+
+	return Controller.extend("QuickStartApplication.controller.View1", {
+		onInit: function() {
+					
+//			var userProfileModel = new JSONModel({
+//				userName: "Wang Tiven"
+//			});
+//			
+//			this.getView().setModel(userProfileModel);
+		}
+		
+	});
+
+});
